30e809f80e7a2402b60bdbf3e53112169c5a901c
Mahesh <mahesh@mahesh.ws>
2013-11-04 13:25:57 +0530

    initial release(unfinished)

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author Maxxx
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Calc abc=new Calc();
+       abc.setVisible(true);
+    }
+}
